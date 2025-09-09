fd7a5e5386f0d859f6afc59ed1f038bf2402777a
famzyjr <famuyiwaahmed132@gmail.com>
2025-09-09 20:46:39 +0100

    ininital coommit: newNavbar

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,35 +1,26 @@
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom'
 
 function NavScrollExample() {
   return (
-<Navbar expand="lg" className='navbar'>
-
-      <Container fluid>
-        <Navbar.Collapse id="navbarScroll">
-          <Nav className='navwords'>
-        <div className='navlinks'>
-            <ul>
-              <Nav.Link as={Link} to="/" id="basic-nav-dropdown">Home</Nav.Link>
-            <Nav.Link as={Link} to="./About" id="basic-nav-dropdown">About</Nav.Link>
-            <Nav.Link as={Link} to="./Service" id="basic-nav-dropdown">Service</Nav.Link>
-            <Nav.Link as={Link} to="./Team" id="basic-nav-dropdown">Team</Nav.Link>
-            <Nav.Link as={Link} to="./Contact" id="basic-nav-dropdown">Contact</Nav.Link>
-            </ul>
-           <div>
-                <button className='navbutton'><h4 className='navv'>Book A Meeting</h4></button>
+    <>   
+      <nav>
+     <div className='logoHeader'><h1>GLITCH</h1></div>
+        <div className="navContainer">
+          <div className='navcon'>
+            <Link to="/" className="nav-link">Home</Link>
+            <Link to="/about" className="nav-link">About</Link>
+            <Link to="/service" className="nav-link">Service</Link>
+            <Link to="/Team" className="nav-link">Team</Link>
+            <Link to="/contact" className="nav-link">contact</Link>
+            <div className="navbtn">
+              <button>Book a meeting</button>
             </div>
+          </div>
         </div>
-          </Nav>
-          
-            
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+
+      </nav>
+    </>
+
   );
 }
 
