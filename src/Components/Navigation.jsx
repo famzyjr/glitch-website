49cb3080985bfe1e9fc4e36d181351edc12dbcac
famzyjr <famuyiwaahmed132@gmail.com>
2025-09-09 18:38:40 +0100

    Initial commit: restructued the navbar

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -8,19 +8,22 @@ import { Link } from 'react-router-dom'
 function NavScrollExample() {
   return (
 <Navbar expand="lg" className='navbar'>
+
       <Container fluid>
         <Navbar.Collapse id="navbarScroll">
           <Nav className='navwords'>
         <div className='navlinks'>
-            <Nav.Link as={Link} to="/" id="basic-nav-dropdown">Home</Nav.Link>
+            <ul>
+              <Nav.Link as={Link} to="/" id="basic-nav-dropdown">Home</Nav.Link>
             <Nav.Link as={Link} to="./About" id="basic-nav-dropdown">About</Nav.Link>
             <Nav.Link as={Link} to="./Service" id="basic-nav-dropdown">Service</Nav.Link>
             <Nav.Link as={Link} to="./Team" id="basic-nav-dropdown">Team</Nav.Link>
             <Nav.Link as={Link} to="./Contact" id="basic-nav-dropdown">Contact</Nav.Link>
-        </div>
-            <div>
+            </ul>
+           <div>
                 <button className='navbutton'><h4 className='navv'>Book A Meeting</h4></button>
             </div>
+        </div>
           </Nav>
           
             
